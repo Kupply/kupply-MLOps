ff2a319252b6010f53c5674f180857f62fc18296
dhdbsrlw <dhdbsrlw@korea.ac.kr>
2023-12-04 20:49:59 +0900

    Add: dags

--- Airflow/dags/train_dag.py
+++ Airflow/dags/train_dag.py
@@ -1,16 +1,44 @@
-# 1. Train Automation System 구축 
-
 import os
-import pendulum # Python datetime module 조작 목적
+import pendulum  # Python datetime module 조작 목적
 from datetime import timedelta
 
-from airflow import DAG
-from airflow.operators.python import PythonOperator
+###
+import pandas as pd
+import numpy as np
+import math
+from torch.utils.data import Dataset, DataLoader
+from transformers import BertTokenizer, BertForSequenceClassification, AdamW
+from os import path
+from datetime import datetime
 
-# Airflow 폴더 내에서 실행 필요, 오류 시 경로 수정
-from src.preprocess_algo import inferenceDataset # 클래스 import 
-from src.preprocess_algo import preprocess_raw_data, tokenize_processed_data, get_dataloader # 함수 import 
-from src.train_algo import 
+from kobert_tokenizer import KoBERTTokenizer
+from transformers.optimization import AdamW, get_cosine_schedule_with_warmup
+from tqdm import tqdm
+import sentencepiece
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+import gluonnlp as nlp
+import numpy as np
+from tqdm.notebook import tqdm
+
+###
+
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator, BranchPythonOperator, ShortCircuitOperator
+
+
+from src.preprocess_algo import trainDataset  # 클래스 import
+from src.preprocess_algo import get_tokenizer, preprocess_dataset, tokenize_dataset, get_labels, split_dataset, get_train_dataset, get_dataloader  # 함수 import
+from src.train_algo import get_model_config, get_model, get_train_config, get_optimizer, get_scheduler_config, get_scheduler, model_train
+
+
+#######################################################
+
 
 seoul_time = pendulum.timezone('Asia/Seoul')
 dag_name = os.path.basename(__file__).split('.')[0]
@@ -19,49 +47,80 @@ dag_name = os.path.basename(__file__).split('.')[0]
 default_args = {
     'owner': 'kupply',
     'retries': 3,
-    'retry_delay': timedelta(minutes=1) 
+    'retry_delay': timedelta(minutes=1)
     # 실패 시 1분 간격으로 retry (추후 수정 필요)
 }
 
-# 단순 복붙 상태, train_algo.py 작성 이후 수정 필요
+# 단순 복붙 상태
 with DAG(
     dag_id=dag_name,
     default_args=default_args,
     description='2023-2 DevKor MLOps 프로젝트',
-    schedule_interval=timedelta(minutes=10), # 수정 필요
-    start_date=pendulum.datetime(2023, 12, 1, tz=seoul_time), # 수정 필요
-    catchup=False, # 과거(= 과거 start_date)의 dag 실행 여부
-    tags=['kupply', 'classification'] # 필요 시 추가
+    schedule_interval=timedelta(minutes=10),  # 수정 필요
+    start_date=pendulum.datetime(2023, 12, 1, tz=seoul_time),  # 수정 필요
+    catchup=False,  # 과거(= 과거 start_date)의 dag 실행 여부
+    tags=['kupply', 'classification']  # 필요 시 추가
 ) as dag:
-    get_market_fundamental_task = PythonOperator(
-        task_id='get_market_fundamental_task',
-        python_callable=get_market_fundamental,
+
+    get_model_task = PythonOperator(
+        task_id='get_model_task',
+        python_callable=get_model,
     )
-    
-    select_columns_task = PythonOperator(
-        task_id='select_columns_task',
-        python_callable=select_columns,
+
+    get_tokenizer_task = PythonOperator(
+        task_id='get_tokenizer_task',
+        python_callable=get_tokenizer,
     )
-    
-    remove_row_fundamental_task = PythonOperator(
-        task_id='remove_row_fundamental_task',
-        python_callable=remove_row_fundamental,
+
+    get_labels_task = PythonOperator(
+        task_id='get_labels_task',
+        python_callable=get_labels,  # 함수에 인자(sample) 필요
     )
-    
-    rank_fundamental_task = PythonOperator(
-        task_id='rank_fundamental_task',
-        python_callable=rank_fundamental,
+
+    preprocess_dataset_task = PythonOperator(
+        task_id='preprocess_dataset_task',
+        python_callable=preprocess_dataset,  # 함수에 인자(sample) 필요
     )
-    
-    select_stock_task = PythonOperator(
-        task_id='select_stock_task',
-        python_callable=select_stock,
+
+    tokenize_dataset_task = PythonOperator(
+        task_id='tokenize_dataset_task',
+        python_callable=tokenize_dataset,  # 함수에 인자(sample) 필요
     )
-    
-    print_selected_stock_task = PythonOperator(
-        task_id='print_selected_stock_task',
-        python_callable=print_selected_stock,
+
+    get_train_dataset_task = PythonOperator(
+        task_id='get_train_dataset_task',
+        python_callable=get_train_dataset,  # 함수에 인자(sample) 필요
     )
-    
+
+    split_dataset_task = PythonOperator(
+        task_id='split_dataset_task',
+        python_callable=split_dataset,  # 함수에 인자(sample, ratio) 2개 필요
+    )
+
+    get_train_config_task = PythonOperator(
+        task_id='get_train_config_task',
+        python_callable=get_train_config,
+    )
+
+    get_optimizer_task = PythonOperator(
+        task_id='get_optimizer_task',
+        python_callable=get_optimizer,
+    )
+
+    get_scheduler_config_task = PythonOperator(
+        task_id='get_scheduler_config_task',
+        python_callable=get_scheduler_config,  # 함수에 인자(train_dataloader) 필요
+    )
+
+    get_scheduler_task = PythonOperator(
+        task_id='get_scheduler_task',
+        python_callable=get_scheduler,
+    )
+
+    model_train_task = PythonOperator(
+        task_id='model_train_task',
+        python_callable=model_train,  # 함수에 인자(train_dataloader) 필요
+    )
+
     # 실행 순서대로 나열
-    get_market_fundamental_task >> select_columns_task >> remove_row_fundamental_task >> rank_fundamental_task >> select_stock_task >> print_selected_stock_task
+    get_model_task >> get_tokenizer_task >> get_labels_task >> preprocess_dataset_task >> tokenize_dataset_task >> get_train_dataset_task >> split_dataset_task >> get_train_config_task >> get_optimizer_task >> get_scheduler_config_task >> get_scheduler_task >> model_train_task
