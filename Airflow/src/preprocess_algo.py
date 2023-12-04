d3bbc1a56cde49dea81336ab953c1ad7869fc70a
dhdbsrlw <dhdbsrlw@korea.ac.kr>
2023-12-04 18:31:16 +0900

    Add: algorithm

--- Airflow/src/preprocess_algo.py
+++ Airflow/src/preprocess_algo.py
@@ -1,20 +1,26 @@
-from kobert_tokenizer import KoBERTTokenizer # 별도 처리 필요
+"""
+[ 수정 필요 사항 ] 
+1. kobert_tokenizer S3 에 업로드 후 로드 (oth. 로컬에서 로드)
+"""
+
+from kobert_tokenizer import KoBERTTokenizer
 import torch
 from torch.utils.data import Dataset, DataLoader
 
-# tokenizer = KoBERTTokenizer.from_pretrained('skt/kobert-base-v1')
-# Train 용 DataLoader 정의 (train 파일에서 함께 import)
+
+# Train 용 DataLoader 정의
 class trainDataset(Dataset):
     def __init__(self, content, labels, attention_masks):
         self.content = content
         self.labels = labels
         self.attention_masks = attention_masks
-        self.num_classes = 2 # 훈련 데이터셋에 라벨이 2 class 인 관계로, 우선 2 class 로 설정 (코드 수정 시, 위 토크나이저 임포트 코드와 함께 바꿔주기)
+        # 훈련 데이터셋에 라벨이 2 class 인 관계로, 우선 2 class 로 설정 (코드 수정 시, 위 토크나이저 임포트 코드와 함께 바꿔주기)
+        self.num_classes = 2
         self.one_hot_labels = torch.zeros(len(labels), self.num_classes)
         for i, label in enumerate(self.labels):
-              # print(i,label)
-              self.one_hot_labels[i, label] = 1
-        
+            # print(i,label)
+            self.one_hot_labels[i, label] = 1
+
     def __len__(self):
         return len(self.content)
 
@@ -25,69 +31,69 @@ class trainDataset(Dataset):
             'attention_mask': self.attention_masks[idx],
             'gt_label': self.labels[idx]
         }
-        
 
-# Train 용 전처리 함수
+
+def get_tokenizer():
+    tokenizer = KoBERTTokenizer.from_pretrained('skt/kobert-base-v1')
+    return tokenizer
+
+
 def preprocess_dataset(sample):
     preprocessed = []
-    
     for _, row in sample.iterrows():
         text = f"First Major is {row['firstMajor']}, Apply Grade is {row['applyGrade']}, Apply Major is {row['applyMajor']}, Apply Semester is {row['applySemester']}, GPA is {row['applyGPA']}, Pass is {row['pass']}"
         preprocessed_text = "[CLS] " + text + " [SEP]"
         preprocessed.append(preprocessed_text)
-        
     return preprocessed
 
 
-# 토크나이저 함수
-def tokenize_dataset(processed_str):
+def tokenize_dataset(sample):
+    tokenizer = get_tokenizer
     tokenized_data = tokenizer.batch_encode_plus(
-        processed_str, 
+        sample,
         add_special_tokens=True,
         padding='longest',
         truncation=True,
-        max_length=128, # 기존 256 에서 수정
+        max_length=128,  # 기존 256 에서 수정
         return_attention_mask=True,
         return_tensors='pt'
     )
     return tokenized_data
 
 
-def get_labels():
-    df = # raw_data (DB 혹은 CSV 파일)
-    labels = list(map(int,df['pass'].tolist()))
+def get_labels(sample):
+    # df = # raw_data (DB 혹은 CSV 파일)
+    labels = list(map(int, sample['pass'].tolist()))
     print(labels)
-    
     return labels
 
 
-def split_dataset(dataset, ratio):
+def split_dataset(sample, ratio):
     # dataset = # raw_data
     # ratio = # ratio
-    train_size = int(ratio * len(dataset))
-    val_size = len(dataset) - train_size
-    train_dataset, val_dataset = torch.utils.data.random_split(dataset, [train_size, val_size])
-    
+    train_size = int(ratio * len(sample))
+    val_size = len(sample) - train_size
+    train_dataset, val_dataset = torch.utils.data.random_split(
+        sample, [train_size, val_size])
     return train_dataset, val_dataset
 
 
-def get_train_dataset(): 
-    raw_data = # raw_data (DB 혹은 CSV 파일)
-    tokenized_data = # 추후 위 함수와 연결
+def get_train_dataset(sample):
     labels = get_labels()
-    training_dataset = trainDataset(
-        content=tokenized_data['input_ids'],
+    train_dataset = trainDataset(
+        content=sample['input_ids'],
         labels=labels,
-        attention_masks=tokenized_data['attention_mask'],
+        attention_masks=sample['attention_mask'],
     )
-    return training_dataset
-    
-    
-def get_dataloader():
-    dataset = # raw_data
-    train_dataset, val_dataset = split_dataset(dataset, 0.8)
-    batch_size = 8
-    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False) # val 데이터셋은 shuffle 하면 안된다.
+    return train_dataset
 
+
+def get_dataloader(sample):
+    train_dataset, val_dataset = split_dataset(sample, 0.8)
+    batch_size = 8
+    train_dataloader = DataLoader(
+        train_dataset, batch_size=batch_size, shuffle=True)
+    # val 데이터셋은 shuffle 하면 안된다.
+    val_dataloader = DataLoader(
+        val_dataset, batch_size=batch_size, shuffle=False)
     return train_dataloader, val_dataloader
--- Airflow/src/train_algo.py
+++ Airflow/src/train_algo.py
@@ -1,94 +1,133 @@
-# 기본적인 라이브러리
+"""
+[ 수정 필요 사항 ] 
+1. 체크포인트 저장 경로 S3 로 수정 필요
+"""
+
 import pandas as pd
 import numpy as np
-import math
 import torch
-from torch.utils.data import Dataset, DataLoader
-from transformers import BertTokenizer, BertForSequenceClassification, AdamW
-from os import path
-from datetime import datetime
+import math
+import os
 
-# for koBERT
-from kobert_tokenizer import KoBERTTokenizer
+from os import path
+from transformers import BertTokenizer, BertForSequenceClassification, AdamW
+
 from transformers.optimization import AdamW, get_cosine_schedule_with_warmup
 from tqdm import tqdm
-import sentencepiece
+from tqdm.notebook import tqdm
 from torch import nn
 import torch.nn.functional as F
 import torch.optim as optim
-import gluonnlp as nlp
-from tqdm.notebook import tqdm
 
-tokenizer = KoBERTTokenizer.from_pretrained('skt/kobert-base-v1')
-model = BertForSequenceClassification.from_pretrained('skt/kobert-base-v1',num_labels=2)
 
-# Trainig Config
-# 딥러닝...이지만... 샘플 데이터수가 작은 관계로 전반적으로 작은 값으로 설정
-epochs = 50
-warmup_ratio = 0.1
-lr = 2e-5
-grad_clip = 1.0
-train_log_interval = 30 # train 이 100번 이루어질 때마다 logging
-# validation_interval = 1000
-save_interval = 60 # save point는 1000번의 train
+def get_model_config():
+    model_path = 'skt/kobert-base-v1'
+    num_labels = 2
+    return model_path, num_labels
 
-# Device
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model.to(device) # GPU 에 모델 올리기
 
-# Optimizer
-optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
+def get_model():
+    model_path, num_labels = get_model_config
+    model = BertForSequenceClassification.from_pretrained(
+        model_path, num_labels=num_labels)
+    return model
 
-# scheduler
-data_len = len(train_dataloader)
-num_train_steps = int(data_len / batch_size * epochs)
-num_warmup_steps = int(num_train_steps * warmup_ratio)
-scheduler = get_cosine_schedule_with_warmup(optimizer, num_warmup_steps=num_warmup_steps, num_training_steps=num_train_steps)
+
+def get_train_config():
+    epochs = 50
+    lr = 2e-5
+    grad_clip = 1.0
+    train_log_interval = 30
+    # validation_interval = 1000
+    save_interval = 60
+    return epochs, lr, grad_clip, train_log_interval, save_interval  # 총 5가지
+
+
+def get_optimizer():
+    model = get_model
+    _, lr, _, _, _ = get_train_config
+    optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
+    return optimizer
+
+
+def get_scheduler_config(train_dataloader):
+    epochs, _, _, _, _ = get_train_config
+    warmup_ratio = 0.1
+    data_len = len(train_dataloader)
+    num_train_steps = int(data_len / epochs)
+    num_warmup_steps = int(num_train_steps * warmup_ratio)
+    return num_train_steps, num_warmup_steps
+
+
+def get_scheduler():
+    num_train_steps, num_warmup_steps = get_scheduler_config
+    optimizer = get_optimizer
+    scheduler = get_cosine_schedule_with_warmup(
+        optimizer, num_warmup_steps=num_warmup_steps, num_training_steps=num_train_steps)
+    return scheduler
 
 # Train 함수 정의
-def model_train(model, optimizer, scheduler, train_dataloader, device):
-    model.to(device)  # 모델 학습을 설정된 device (CPU, cuda) 위에서 진행하도록 설정
 
-    model.train() # 모델을 학습 모드로 전환
+
+def model_train(train_dataloader):
+
+    # model config
+    model = get_model
+    optimizer = get_optimizer
+    scheduler = get_optimizer
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # train config
+    epochs, lr, grad_clip, train_log_interval, save_interval = get_train_config
+
+    # 모델 학습을 설정된 device (CPU, cuda) 위에서 진행하도록 설정
+    model.to(device)
+
+    # 모델을 학습 모드로 전환
+    model.train()
     loss_list_between_log_interval = []
 
+    # 모델 이터레이션
     for epoch_id in range(epochs):
         for step_index, batch_data in tqdm(enumerate(train_dataloader), f"[TRAIN] Epoch:{epoch_id+1}", total=len(train_dataloader)):
-                global_step = len(train_dataloader) * epoch_id + step_index + 1
+            global_step = len(train_dataloader) * epoch_id + step_index + 1
 
-                # Add a condition to break the loop if we've gone through all data points
-                if step_index * batch_size >= len(dataset):
-                  continue
+            # Add a condition to break the loop if we've gone through all data points
+            if step_index >= len(train_dataloader):  # len(dataset):
+                continue
 
-                optimizer.zero_grad()
-                contents = batch_data['content']
-                labels = batch_data['label']
-                attention_masks = batch_data['attention_mask']
+            optimizer.zero_grad()
+            contents = batch_data['content']
+            labels = batch_data['label']
+            attention_masks = batch_data['attention_mask']
 
-                # 모델의 input들을 device(GPU)와 호환되는 tensor로 변환
-                contents = contents.to(device)
-                labels = labels.to(device)
-                attention_masks = attention_masks.to(device)
+            # 모델의 input들을 device(GPU)와 호환되는 tensor로 변환
+            contents = contents.to(device)
+            labels = labels.to(device)
+            attention_masks = attention_masks.to(device)
 
-                model_outputs = model(
-                    contents, token_type_ids=None, attention_mask=attention_masks, labels=labels
-                    )
+            model_outputs = model(
+                contents, token_type_ids=None, attention_mask=attention_masks, labels=labels
+            )
 
-                loss = model_outputs.loss
-                loss.backward()
+            loss = model_outputs.loss
+            loss.backward()
 
-                torch.nn.utils.clip_grad_norm_(model.parameters(), grad_clip)
-                optimizer.step()
-                scheduler.step()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), grad_clip)
+            optimizer.step()
+            scheduler.step()
 
-                # for logging
-                loss_list_between_log_interval.append(model_outputs.loss.item())
+            # for logging
+            loss_list_between_log_interval.append(model_outputs.loss.item())
 
-                #if global_step % train_log_interval == 0:
+            # if global_step % train_log_interval == 0:
 
         mean_loss = np.mean(loss_list_between_log_interval)
 
         # 콘솔 출력
+        print(f"EP:{epoch_id} global_step:{global_step} ")
+        print(f"loss:{mean_loss:.4f} perplexity:{math.exp(mean_loss):.4f}")
+
         # logger.info(
         #     f"EP:{epoch_id} global_step:{global_step} "
         #     f"loss:{mean_loss:.4f} perplexity:{math.exp(mean_loss):.4f}"
@@ -101,11 +140,13 @@ def model_train(model, optimizer, scheduler, train_dataloader, device):
 
         # 각 epoch 마다 모델 저장
         state_dict = model.state_dict()
-        model_path = os.path.join('/content/drive/MyDrive/Colab Notebooks/kupply-MLOps/checkpoint/train_1', f"kupply_epoch_{epoch_id}.pth")
+        model_path = os.path.join(
+            './', f"kupply_epoch_{epoch_id}.pth")
         # logger.info(f"global_step: {global_step} model saved at {model_path}")
         torch.save(state_dict, model_path)
 
-    return model
+    # return model
+
 
 # Let's Training
-model_v1 = model_train(model, optimizer, scheduler, train_dataloader, device)
+# model_v1 = model_train(model, optimizer, scheduler, train_dataloader, device)
