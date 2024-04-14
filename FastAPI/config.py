1d7cfb4f094130d26882c72a4ceadea0f01bba78
YoonJunHyeok <jhyoon1607@gmail.com>
2024-04-14 19:34:55 +0900

    feat: get pytorch mode from aws s3

--- FastAPI/config.py
+++ FastAPI/config.py
@@ -1,9 +1,21 @@
-# 모델 Inference 에 필요한 파라미터 지정
+import os
+import boto3
 
 class Config():
+    def __init__(self, aws_bucket_name, aws_key):
+        self.aws_bucket_name = aws_bucket_name
+        self.aws_key = aws_key
+        self.model_path = "./trained_model/downloaded_model.pth" 
+        self._download_model()
 
-    def __init__(self, model_path):
-        self.model_path = model_path
-        # self.batch_size = batch_size
-        # self.gpu_id = gpu_id
-        
+    def _download_model(self):
+        if not os.path.exists(self.model_path):
+            client = boto3.client('s3',
+                                  aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+                                  aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+                                  region_name=os.getenv('AWS_DEFAULT_REGION')
+                                  )
+            client.download_file(self.aws_bucket_name, self.aws_key, self.model_path)
+
+    def get_model_path(self):
+        return self.model_path
